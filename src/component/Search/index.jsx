ef1f72ca2c1f63ab37540eb9706155b0d062e037
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-13 02:42:27 +0300

    edit in search Relates #5

--- src/component/Search/index.jsx
+++ src/component/Search/index.jsx
@@ -5,25 +5,27 @@ function Search() {
   const [inputValue, setinputValue] = useState('');
   const [data, setData] = useState([]);
   const inputHandle = (e) => setinputValue(e.target.value);
+
   useEffect(() => {
     const controller = new AbortController();
-    const signal = controller.signal;  
-    fetch(`https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=${inputValue}`, { signal: signal })
+    const { signal } = controller;
+    fetch(`https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=${!inputValue ? 'search' : inputValue}`, { signal })
       .then((res) => res.json())
       .then((results) => {
+        console.log('data from api', results);
         setData(results.results);
       });
 
     return () => { controller.abort(); setData([]); };
   }, [inputValue]);
+
   return (
     <>
       <input type="text" onChange={inputHandle} value={inputValue} placeholder="Search" />
-      {data.map((film) => console.log(film))}
+      {console.log(data)}
+      {data.map((film) => <Card key={film.id} poster_path={film.poster_path} title={film.title} vote_average={film.vote_average} overview={film.overview} />)}
     </>
   );
 }
 
 export default Search;
-{/* <Card poster_path={film.poster_path} title={film.title} vote_average={film.vote_average} overview={film.overview} /> */}
-
