67489f9ea29dbb8d72eecf6980cfdcce7a207a0e
Rand <randsohail98@gmail.com>
2022-04-12 22:43:37 +0300

    make the basic routes for the home page and search page Relates #3

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>test2</p>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/component/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <>
+      <h1>Search</h1>
+      <p>test2</p>
+    </>
+  );
+}
+
+export default Search;
