c6cb58e302ba697a5a7583ee44dcef6e304cf4fe
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-13 01:56:54 +0300

    search component Relates#5

--- src/component/Search/index.jsx
+++ src/component/Search/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../Card';
 
 function Search() {
+  const [inputValue, setinputValue] = useState('');
+  const [data, setData] = useState([]);
+  const inputHandle = (e) => setinputValue(e.target.value);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;  
+    fetch(`https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=${inputValue}`, { signal: signal })
+      .then((res) => res.json())
+      .then((results) => {
+        setData(results.results);
+      });
+    return () => { controller.abort(); setData([]); };
+  }, [inputValue]);
   return (
     <>
-      <h1>Search</h1>
-      <p>test2</p>
+      <input type="text" onChange={inputHandle} value={inputValue} placeholder="Search" />
+      {data.map((film) => {
+        return (
+        <Card poster_path={film.poster_path} title={film.title} vote_average={film.vote_average} overview={film.overview} />
+        );
+      })}
     </>
   );
 }
