c8af0b4f45d34ea96e9c94f6f946fd33bf56eced
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-13 21:22:54 +0300

    Card component Relates #12

--- src/component/Card/index.jsx
+++ src/component/Card/index.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CardDescription from '../CardDescription';
+import './Card.css'
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-function Card({ poster_path, title, vote_average, overview }) {
+function Card({film}) {
+    // console.log(film)
+    const[description, setDescription] = useState(false);
+    const descriptionHandle = () => setDescription(true);
     return (
-        <div>
-            <img src={poster_path} alt='Image not found' />
-            <h2>{title}</h2>
-            <p>{vote_average}</p>
-            <p>{overview}</p>
-        </div>
-
+            <div className='card'>
+                <img src={IMGPATH + film.poster_path} alt='Image not found' />
+                <div className='info'>
+                    <h2>{film.title}</h2>
+                    <i class="fa-solid fa-star"></i>
+                    <p className='vote-class'>{film.vote_average}</p>
+                    <button className='btn' onClick={descriptionHandle} >Read More</button>
+                    {description && 
+                    <CardDescription detailsAfilm={film} /> 
+                     }
+                    {/* <p>{overview}</p> */}
+                </div>
+            </div>
     );
 }
 
